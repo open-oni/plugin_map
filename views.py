af34254c055c6eb04cde981981c2b358b5442da1
Greg Tunink <techgique@gmail.com>
2019-04-01 14:36:04 -0500

    Fix unwanted display of cities with no issues

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from core.models import Place
 
 def map(request):
-    places = Place.objects.all().order_by("city")
+    places = Place.objects.filter(titles__has_issues=1).order_by("city").distinct()
 
     page_title = "Newspapers by City"
     return render(request, 'map.html', locals())
