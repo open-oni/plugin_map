77d38a94e954cb07f2abdcf5527af4c3255e6825
Greg Tunink <techgique@gmail.com>
2019-07-19 12:33:36 -0500

    Wrap initial JS to execute after DOM load

--- static/js/map.js
+++ static/js/map.js
@@ -1,61 +1,63 @@
-var map = L.map('map_container').setView(new L.LatLng(startLat, startLong), startZoom);
+$(function() {
+  var map = L.map('map_container').setView(new L.LatLng(startLat, startLong), startZoom);
 
-// make base layer
-var stamenLayer = new L.StamenTileLayer(stamenType);
-map.addLayer(stamenLayer);
+  // make base layer
+  var stamenLayer = new L.StamenTileLayer(stamenType);
+  map.addLayer(stamenLayer);
 
-// Uncomment the below if you would like to get the historical overlay working
+  // Uncomment the below if you would like to get the historical overlay working
 
-// the historical overlay
-// requires a tiling service
-// var overlay = new L.TileLayer.WMS("http://server:8080/geoserver/gwc/service/wms", {
-//           layers: 'states:Nebraska_wma',
-//           format: 'image/png',
-//           transparent: true
-// });
-// overlay.addTo(map);
-//
-// add a toggle for the two layers
-// L.control.layers({ "Plain" : stamenLayer, "Historical" : overlay }).addTo(map);
+  // the historical overlay
+  // requires a tiling service
+  // var overlay = new L.TileLayer.WMS("http://server:8080/geoserver/gwc/service/wms", {
+  //           layers: 'states:Nebraska_wma',
+  //           format: 'image/png',
+  //           transparent: true
+  // });
+  // overlay.addTo(map);
+  //
+  // add a toggle for the two layers
+  // L.control.layers({ "Plain" : stamenLayer, "Historical" : overlay }).addTo(map);
 
 
-var makeLinks = function(city, papers) {
-  if (papers) {
-    var res = "<h4>"+city+"</h4>";
-    for (var paper in papers) {
-      res += singleLink(paper, papers[paper])
+  var makeLinks = function(city, papers) {
+    if (papers) {
+      var res = "<h4>"+city+"</h4>";
+      for (var paper in papers) {
+        res += singleLink(paper, papers[paper])
+      }
+      return res;
+    } else {
+      return "No papers found for this city";
     }
-    return res;
-  } else {
-    return "No papers found for this city";
-  }
-};
+  };
 
-var singleLink = function(name, id) {
-  return "<li class='map_paper_link'>"
-         +"<a href='../lccn/"
-         +id+"/'>"+name+"</a></li>";
-};
+  var singleLink = function(name, id) {
+    return "<li class='map_paper_link'>"
+           +"<a href='../lccn/"
+           +id+"/'>"+name+"</a></li>";
+  };
 
-var circleMarkers = {
-  radius: 8,
-  fillColor: markerColor,
-  color: markerColor,
-  weight: 1,
-  opacity: 1,
-  fillOpacity: .8
-};
+  var circleMarkers = {
+    radius: 8,
+    fillColor: markerColor,
+    color: markerColor,
+    weight: 1,
+    opacity: 1,
+    fillOpacity: .8
+  };
 
-cities.forEach(function(city) {
-  var marker = L.circleMarker(city["latlong"], circleMarkers);
-  marker.addTo(map);
-  marker.bindPopup(makeLinks(city["name"], city["papers"]));
-  
-  // add a mouseover that waits for you to click away before closing
-  marker.on('mouseover', function(e) {
-    this.openPopup();
-  });
-  marker.on('click', function(e) {
-    this.closePopup();
+  cities.forEach(function(city) {
+    var marker = L.circleMarker(city["latlong"], circleMarkers);
+    marker.addTo(map);
+    marker.bindPopup(makeLinks(city["name"], city["papers"]));
+    
+    // add a mouseover that waits for you to click away before closing
+    marker.on('mouseover', function(e) {
+      this.openPopup();
+    });
+    marker.on('click', function(e) {
+      this.closePopup();
+    });
   });
 });
